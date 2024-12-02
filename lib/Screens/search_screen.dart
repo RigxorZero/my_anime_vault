4755573210ecd63ddf31a1d0b1ae10038f78e905
RigxorZero <127953440+RigxorZero@users.noreply.github.com>
2024-12-02 19:55:28 -0300

    Mejora a búsqueda
    Mejore la búsqueda para trabajar con filtros

--- lib/Screens/search_screen.dart
+++ lib/Screens/search_screen.dart
@@ -17,6 +17,43 @@ class _SearchScreenState extends State<SearchScreen> {
   bool searchPressed = false;
   final TextEditingController _searchController = TextEditingController();
 
+  String selectedGenre = 'All';
+  String selectedSeason = 'All';
+  String selectedYear = 'All';
+
+  static const Map<String, String> genreTranslations = {
+    "Action": "Acción",
+    "Adventure": "Aventura",
+    "Comedy": "Comedia",
+    "Drama": "Drama",
+    "Ecchi": "Ecchi",
+    "Fantasy": "Fantasía",
+    "Hentai": "Hentai",
+    "Horror": "Horror",
+    "Mahou Shoujo": "Mahou Shoujo",
+    "Mecha": "Mecha",
+    "Music": "Música",
+    "Mystery": "Misterio",
+    "Psychological": "Psicológico",
+    "Romance": "Romance",
+    "Sci-Fi": "Ciencia Ficción",
+    "Slice of Life": "Slice of Life",
+    "Sports": "Deportes",
+    "Supernatural": "Sobrenatural",
+    "Thriller": "Thriller"
+  };
+
+  static const Map<String, String> seasonTranslations = {
+    "WINTER": "Invierno",
+    "SPRING": "Primavera",
+    "SUMMER": "Verano",
+    "FALL": "Otoño"
+  };
+
+  List<String> get genres => ['All'] + genreTranslations.values.toList();
+  List<String> get seasons => ['All'] + seasonTranslations.values.toList();
+  List<String> get years => ['All'] + List.generate(50, (index) => (2023 - index).toString());
+
   @override
   void initState() {
     super.initState();
@@ -35,56 +72,106 @@ class _SearchScreenState extends State<SearchScreen> {
     );
   }
 
-  String get searchAnimeQuery {
-    return """
-query (\$search: String, \$page: Int = 1, \$perPage: Int = 50) {
-  Page(page: \$page, perPage: \$perPage) {
-    media(search: \$search, type: ANIME, sort: POPULARITY_DESC) {
-      id
-      title {
-        romaji
-        english
-        native
-      }
-      coverImage {
-        large
-      }
-      episodes
-      nextAiringEpisode {
-        episode
-        airingAt
-      }
-      genres
-      description
-      startDate {
-        year
-        month
-        day
-      }
-      endDate {
-        year
-        month
-        day
-      }
-      source
-      status
-      season
-      seasonYear
-      studios {
-        nodes {
-          name
+    String get searchAnimeQuery {
+    if (searchQuery.isNotEmpty) {
+      return """
+  query (\$search: String, \$page: Int = 1, \$perPage: Int = 50) {
+    Page(page: \$page, perPage: \$perPage) {
+      media(search: \$search, type: ANIME, sort: POPULARITY_DESC) {
+        id
+        title {
+          romaji
+          english
+          native
+        }
+        coverImage {
+          large
+        }
+        episodes
+        nextAiringEpisode {
+          episode
+          airingAt
+        }
+        genres
+        description
+        startDate {
+          year
+          month
+          day
+        }
+        endDate {
+          year
+          month
+          day
+        }
+        source
+        status
+        season
+        seasonYear
+        studios {
+          nodes {
+            name
+          }
         }
       }
     }
   }
-}
-""";
+  """;
+    } else {
+      String genreFilter = selectedGenre == 'All' ? '' : 'genre_in: ["${genreTranslations.entries.firstWhere((entry) => entry.value == selectedGenre).key}"]';
+      String seasonFilter = selectedSeason == 'All' ? '' : 'season: ${seasonTranslations.entries.firstWhere((entry) => entry.value == selectedSeason).key}';
+      String yearFilter = selectedYear == 'All' ? '' : 'seasonYear: $selectedYear';
+  
+      return """
+  query (\$page: Int = 1, \$perPage: Int = 50) {
+    Page(page: \$page, perPage: \$perPage) {
+      media(type: ANIME, sort: POPULARITY_DESC, $genreFilter, $seasonFilter, $yearFilter) {
+        id
+        title {
+          romaji
+          english
+          native
+        }
+        coverImage {
+          large
+        }
+        episodes
+        nextAiringEpisode {
+          episode
+          airingAt
+        }
+        genres
+        description
+        startDate {
+          year
+          month
+          day
+        }
+        endDate {
+          year
+          month
+          day
+        }
+        source
+        status
+        season
+        seasonYear
+        studios {
+          nodes {
+            name
+          }
+        }
+      }
+    }
+  }
+  """;
+    }
   }
 
   void _performSearch() {
     setState(() {
       searchQuery = _searchController.text;
-      searchPressed = searchQuery.isNotEmpty;
+      searchPressed = true;
       print("Search query: $searchQuery");
     });
   }
@@ -136,12 +223,103 @@ query (\$search: String, \$page: Int = 1, \$perPage: Int = 50) {
                   ],
                 ),
               ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: DropdownButton<String>(
+                        value: selectedGenre,
+                        onChanged: (String? newValue) {
+                          setState(() {
+                            selectedGenre = newValue!;
+                          });
+                        },
+                        items: genres.map<DropdownMenuItem<String>>((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                        isExpanded: true,
+                        hint: const Text('Género'),
+                        selectedItemBuilder: (BuildContext context) {
+                          return genres.map<Widget>((String value) {
+                            return Text(
+                              value == 'All' ? 'Género' : value,
+                              style: TextStyle(color: value == 'All' ? Colors.grey : Colors.black),
+                            );
+                          }).toList();
+                        },
+                      ),
+                    ),
+                    const SizedBox(width: 8),
+                    Expanded(
+                      child: DropdownButton<String>(
+                        value: selectedSeason,
+                        onChanged: (String? newValue) {
+                          setState(() {
+                            selectedSeason = newValue!;
+                          });
+                        },
+                        items: seasons.map<DropdownMenuItem<String>>((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                        isExpanded: true,
+                        hint: const Text('Temporada'),
+                        selectedItemBuilder: (BuildContext context) {
+                          return seasons.map<Widget>((String value) {
+                            return Text(
+                              value == 'All' ? 'Temporada' : value,
+                              style: TextStyle(color: value == 'All' ? Colors.grey : Colors.black),
+                            );
+                          }).toList();
+                        },
+                      ),
+                    ),
+                    const SizedBox(width: 8),
+                    Expanded(
+                      child: DropdownButton<String>(
+                        value: selectedYear,
+                        onChanged: (String? newValue) {
+                          setState(() {
+                            selectedYear = newValue!;
+                          });
+                        },
+                        items: years.map<DropdownMenuItem<String>>((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                        isExpanded: true,
+                        hint: const Text('Año'),
+                        selectedItemBuilder: (BuildContext context) {
+                          return years.map<Widget>((String value) {
+                            return Text(
+                              value == 'All' ? 'Año' : value,
+                              style: TextStyle(color: value == 'All' ? Colors.grey : Colors.black),
+                            );
+                          }).toList();
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
               Expanded(
                 child: searchPressed
                     ? Query(
                         options: QueryOptions(
                           document: gql(searchAnimeQuery),
-                          variables: {'search': searchQuery.toLowerCase(), 'page': 1, 'perPage': 50},
+                          variables: {
+                            'search': searchQuery.isEmpty ? null : searchQuery.toLowerCase(),
+                            'page': 1,
+                            'perPage': 50
+                          },
                         ),
                         builder: (QueryResult result, {VoidCallback? refetch, FetchMore? fetchMore}) {
                           if (result.isLoading) {
